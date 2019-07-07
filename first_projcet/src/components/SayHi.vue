6fddf8547ef65712b8ed006fa40dca1e41bfd4fe
februarysea <857254373@qq.com>
2019-07-07 20:55:52 +0800

    vue learning day1

---
+++ first_projcet/src/components/SayHi.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hi">
+      <input type='button' @click="show_my_value()" value="..."/>
+    </div>
+  <div v-if="a>1"></div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        my_value: '你好，vue！本消息来自于变量'
+      }
+    },
+    methods: {
+      show_my_value: function () {
+        alert('my_value:' + this.my_value);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- first_projcet/src/main.js
+++ first_projcet/src/main.js
@@ -8,8 +8,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#app',  // <div id=app></div>
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>' // ./src/App/vue
 })
